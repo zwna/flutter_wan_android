85aa3eb3047ac92312ff9cf7f9eb80f9d40608a0
joker <3123848646@qq.com>
2019-02-10 15:10:59 +0800

    2019-2-10

--- flutter_wan_android/lib/widget/main_widget.dart
+++ flutter_wan_android/lib/widget/main_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_wan_android/widget/zhi_shi_ti_xi_widget.dart';
 import 'package:flutter_wan_android/widget/xiang_mu_widget.dart';
 import 'package:flutter_wan_android/widget/ge_ren_zhong_xin_widget.dart';
 import 'package:flutter_wan_android/utils/system_chrome_utils.dart';
+import 'package:flutter_wan_android/utils/toast_utils.dart';
 
 class MainWidget extends StatefulWidget {
   @override
@@ -13,6 +14,7 @@ class MainWidget extends StatefulWidget {
 class _MainWidgetState extends State<MainWidget> with AutomaticKeepAliveClientMixin {
 
   int _currentIndex = 0;
+  int _exitTime = 0;
 
   List<Widget> _mainPartWidget;
 
@@ -30,76 +32,86 @@ class _MainWidgetState extends State<MainWidget> with AutomaticKeepAliveClientMi
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body:IndexedStack(
-        children:_mainPartWidget,
-        index:_currentIndex,
-      ),
-      bottomNavigationBar:BottomNavigationBar(
-        key:PageStorageKey(_currentIndex),
-        type:BottomNavigationBarType.fixed,
-        currentIndex:_currentIndex,
-        onTap:(int index){
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon:Icon(
-                Icons.home,
-                color:_currentIndex == 0 ? Colors.green : Colors.black,
-            ),
-            title:Text(
-                '主页',
-                style:TextStyle(
-                    color:_currentIndex == 0? Colors.green:Colors.black,
-                    fontSize:15.0
-                )
-            )
+    return WillPopScope(
+        child: Scaffold(
+          body:IndexedStack(
+            children:_mainPartWidget,
+            index:_currentIndex,
           ),
-          BottomNavigationBarItem(
-            icon:Icon(
-                Icons.widgets,
-                color:_currentIndex == 1? Colors.green:Colors.black,
-            ),
-            title:Text(
-                '知识体系',
-              style: TextStyle(
-                color:_currentIndex == 1? Colors.green:Colors.black,
-                fontSize:15.0,
+          bottomNavigationBar:BottomNavigationBar(
+            key:PageStorageKey(_currentIndex),
+            type:BottomNavigationBarType.fixed,
+            currentIndex:_currentIndex,
+            onTap:(int index){
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon:Icon(
+                    Icons.home,
+                    color:_currentIndex == 0 ? Colors.green : Colors.black,
+                  ),
+                  title:Text(
+                      '主页',
+                      style:TextStyle(
+                          color:_currentIndex == 0? Colors.green:Colors.black,
+                          fontSize:15.0
+                      )
+                  )
               ),
-            )
-          ),
-          BottomNavigationBarItem(
-              icon:Icon(
-                Icons.work,
-                color:_currentIndex == 2? Colors.green:Colors.black,
+              BottomNavigationBarItem(
+                  icon:Icon(
+                    Icons.widgets,
+                    color:_currentIndex == 1? Colors.green:Colors.black,
+                  ),
+                  title:Text(
+                    '知识体系',
+                    style: TextStyle(
+                      color:_currentIndex == 1? Colors.green:Colors.black,
+                      fontSize:15.0,
+                    ),
+                  )
               ),
-              title:Text(
-                '项目',
-                style: TextStyle(
-                  color:_currentIndex == 2? Colors.green:Colors.black,
-                  fontSize:15.0,
+              BottomNavigationBarItem(
+                  icon:Icon(
+                    Icons.work,
+                    color:_currentIndex == 2? Colors.green:Colors.black,
+                  ),
+                  title:Text(
+                    '项目',
+                    style: TextStyle(
+                      color:_currentIndex == 2? Colors.green:Colors.black,
+                      fontSize:15.0,
+                    ),
+                  )
+              ),
+              BottomNavigationBarItem(
+                icon:Icon(
+                  Icons.person,
+                  color:_currentIndex == 3? Colors.green:Colors.black,
+                ),
+                title:Text(
+                  '个人中心',
+                  style: TextStyle(
+                    color:_currentIndex == 3? Colors.green:Colors.black,
+                    fontSize:15.0,
+                  ),
                 ),
               )
+            ],
           ),
-          BottomNavigationBarItem(
-              icon:Icon(
-                Icons.person,
-                color:_currentIndex == 3? Colors.green:Colors.black,
-              ),
-              title:Text(
-                '个人中心',
-                style: TextStyle(
-                  color:_currentIndex == 3? Colors.green:Colors.black,
-                  fontSize:15.0,
-                ),
-              ),
-          )
-        ],
-      ),
-    );
+        ),
+        onWillPop: (){
+          if(DateTime.now().millisecondsSinceEpoch - _exitTime > 2000){
+            _exitTime = DateTime.now().millisecondsSinceEpoch;
+            ToastUtils.showShortToast("再次点击退出APP");
+            return Future.value(false);
+          }else{
+            return Future.value(true);
+          }
+        });
   }
 
   @override
